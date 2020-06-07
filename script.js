867f71f1cd4f0e44f69c6c320f40ff10f3180a04
trinity <twinitiee@gmail.com>
2020-06-06 23:31:15 -0400

    done for the day

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 
+//
 // var slideIndex = 1;
 // showSlides(slideIndex);
 //
@@ -57,18 +57,4 @@ function myFunction() {
   } else {
     x.className = "navigation";
   }
-}
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
 }
